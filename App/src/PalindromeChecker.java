d6f417fed20f55a69cd62714ede007802edbe19c
IshaanK10 <ishaan.khatwani420@gmail.com>
2026-02-18 15:55:44 +0530

    Ishaan Added PalindromeChecker

---
+++ App/src/PalindromeChecker.java
@@ -0,0 +1,7 @@
+public class PalindromeChecker {
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to Palindrome Checker App Management System");
+            System.out.println("Modify this logic to usecase 2");
+        }
+}
